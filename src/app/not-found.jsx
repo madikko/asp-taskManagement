72eced6f258e501f59530307f0f95b881b2e3ddd
Maryam Aliyu Dikko <dikkoma@ndic.gov.ng>
2024-07-08 16:34:46 +0100

    fix: modify the 404 message

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div>
-        Oh oh! The page doesn't exist
+        Oh oh! The page doesn't exist. Make sure you have no typos in your URL
     </div>
   )
 }
