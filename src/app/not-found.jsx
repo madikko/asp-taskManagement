1a2234fe4f374ae4f7e416e76591abff786d6533
Maryam Aliyu Dikko <dikkoma@ndic.gov.ng>
2024-07-08 17:10:08 +0100

    fix: change an apostrophy

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div>
-        Oh oh! The page doesn't exist. Make sure you have no typos in your URL
+       <h1> Oh oh! The page doesn&apos;t exist. Make sure you have no typos in your URL</h1>
     </div>
   )
 }
